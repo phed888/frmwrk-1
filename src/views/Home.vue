7707b538aa2c9976b324f82ddc8e07d703d732eb
Brian Speight <brspeight@expedia.com>
2018-10-24 17:34:01 -0700

    first commit, adding base icon and AC unit icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IconBase height="16" width="16" iconColor="#f00"><IconACUnit /></IconBase>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
+import IconBase from '@/components/icons/IconBase.vue';
+import IconACUnit from '@/components/icons/IconACUnit.vue';
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    IconBase,
+    IconACUnit
   }
 };
 </script>
